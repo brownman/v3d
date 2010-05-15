c573a31ae19d604620b0cfe09afaecd3b98b4911
Konstantin Tennhard <me@t6d.de>
2010-05-15 20:47:03 +0200

    implemented enough wrapper code to let the rspec code pass

--- ext/wrapper.c
+++ ext/wrapper.c
@@ -1,3 +1,11 @@
+#include <ruby.h>
+
 void Init_v3d(void) {
+  VALUE mV3D;
+  VALUE cVector;
+  
+  mV3D    = rb_define_module("V3D");
+  cVector = rb_define_class_under(mV3D, "Vector", rb_cObject);
+  
   return;
 }
